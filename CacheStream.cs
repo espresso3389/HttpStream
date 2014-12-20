075f197da8873d39891e9bc169efd0305755e41c
Takashi Kawasaki <espresso3389@gmail.com>
2014-12-20 20:19:01 +0900

    First commit
    This is a .NET Stream implementation on HTTP, which allows random seek.

---
+++ CacheStream.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace HttpStream
+{
+    public abstract class CacheStream : Stream
+    {
+        public CacheStream(Stream cacheStream, bool ownStream)
+        {
+            if (cacheStream == null)
+            {
+                cacheStream = new MemoryStream();
+                ownStream = true;
+            }
+
+            if (!cacheStream.CanSeek || !cacheStream.CanRead || !cacheStream.CanWrite)
+                throw new ArgumentException("cacheStream should be seekable/readable/writeable.", "cacheStream");
+
+            _cacheStream = cacheStream;
+            _ownStream = ownStream;
+        }
+
+        Stream _cacheStream;
+        bool _ownStream;
+        List<Range> _ranges = new List<Range>();
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            if (disposing)
+            {
+                if (_ownStream && _cacheStream != null)
+                {
+                    _cacheStream.Dispose();
+                    _cacheStream = null;
+                }
+            }
+        }
+
+        #region implemented abstract members of Stream
+
+        public override void Flush()
+        {
+            FlushAsync().Wait();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            return ReadAsync(buffer, offset, count).Result;
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            switch (origin)
+            {
+                case SeekOrigin.Begin:
+                    Position = offset;
+                    break;
+                case SeekOrigin.Current:
+                    Position += offset;
+                    break;
+                case SeekOrigin.End:
+                    Position = Length + offset;
+                    break;
+            }
+            return Position;
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override bool CanRead { get { return true; } }
+
+        public override bool CanSeek { get { return true; } }
+
+        public override bool CanWrite { get { return false; } }
+
+        public override long Length { get { return GetStreamLengthOrFail(long.MaxValue); } }
+
+        public override long Position { get; set; }
+
+        #endregion
+
+        public abstract long GetStreamLengthOrFail(long defValue);
+
+        public abstract Task<Range> LoadAsync(Stream stream, long pos, int size, CancellationToken cancellationToken);
+
+        public override async Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+        {
+            if (count == 0)
+                return 0;
+
+            var end = Position + count;
+
+            var idx = _ranges.BinarySearch(new Range(Position, count));
+            if (idx <= 0)
+                idx = ~idx;
+
+            var firstRange = idx;
+            if (idx > 0)
+            {
+                var prev = _ranges[idx - 1];
+                if (prev.Offset <= Position && Position < prev.End)
+                {
+                    firstRange = --idx;
+                }
+            }
+
+            int allBytes2Read = count;
+            int bytesRead = 0;
+            for (;;)
+            {
+                if (idx == _ranges.Count)
+                {
+                    var r = await LoadAsync(_cacheStream, Position, allBytes2Read, cancellationToken);
+                    updateRenges(firstRange, r);
+                    if (!r.Contains(Position))
+                        return (int)bytesRead;
+
+                    var bytesRemain = (int)(r.Length - (Position - r.Offset));
+                    if (bytesRemain > allBytes2Read)
+                        bytesRemain = allBytes2Read;
+
+                    _cacheStream.Position = Position;
+                    await _cacheStream.ReadAsync(buffer, offset, bytesRemain, cancellationToken);
+
+                    Position += bytesRemain;
+                    bytesRead += bytesRemain;
+                    return (int)bytesRead;
+                }
+
+                int ret, bytes2Read;
+                if (Position < _ranges[idx].Offset)
+                {
+                    bytes2Read = (int)(_ranges[idx].Offset - Position);
+                    var r = await LoadAsync(_cacheStream, Position, bytes2Read, cancellationToken);
+                    updateRenges(firstRange, r);
+                    if (r.Contains(Position))
+                    {
+                        _cacheStream.Position = Position;
+                        if (bytes2Read > allBytes2Read)
+                            bytes2Read = allBytes2Read;
+                        ret = await _cacheStream.ReadAsync(buffer, offset, bytes2Read, cancellationToken);
+                    }
+                    else
+                    {
+                        ret = 0;
+                    }
+                }
+                else
+                {
+                    if (end < _ranges[idx].End)
+                        bytes2Read = (int)(end - Position);
+                    else
+                        bytes2Read = (int)(_ranges[idx].End - Position);
+
+                    _cacheStream.Position = Position;
+                    ret = await _cacheStream.ReadAsync(buffer, offset, bytes2Read, cancellationToken);
+                    idx++;
+                }
+
+                Position += ret;
+                bytesRead += ret;
+                offset += ret;
+                allBytes2Read -= ret;
+                if (ret < bytes2Read || allBytes2Read == 0)
+                    return bytesRead;
+            }
+        }
+
+        void updateRenges(int idx, Range r)
+        {
+            if (r.Length == 0)
+                return;
+
+            var cur = idx >= 0 && idx < _ranges.Count ? _ranges[idx] : null;
+            var prev = idx - 1 >= 0 && idx - 1 < _ranges.Count ? _ranges[idx - 1] : null;
+            if (idx == _ranges.Count)
+            {
+                if (prev != null && _ranges.Count != 0 && prev.End == r.Offset)
+                {
+                    prev.Length = r.End - prev.Offset;
+                    return;
+                }
+                _ranges.Add(r);
+                return;
+            }
+
+            var count = _ranges.Count;
+            if (prev != null && prev.End == r.Offset)
+            {
+                if (r.End < cur.Offset)
+                {
+                    // PPPPRRRR CCCC
+                    prev.Length = r.End - prev.Offset;
+                    return;
+                }
+                else if (r.End == cur.Offset)
+                {
+                    // PPPPRRRRCCCC
+                    prev.Length = cur.End - prev.Offset;
+                    _ranges.RemoveAt(idx);
+                    return;
+                }
+                else
+                {
+                    var end = r.End;
+                    int i;
+                    for(i = idx + 1; i < count; i++)
+                        if (_ranges[i].End > r.End)
+                            break;
+                    if (i == count)
+                        _ranges.RemoveRange(idx, count - idx);
+                    else
+                    {
+                        end = _ranges[i].End;
+                        _ranges.RemoveRange(idx, i - idx + 1);
+                    }
+                    prev.Length = end - prev.Offset;
+                    return;
+                }
+            }
+
+            if (r.End < cur.Offset)
+            {
+                _ranges.Insert(idx, r);
+                return;
+            }
+            else if (r.End <= cur.End)
+            {
+                var offset = (r.Offset < cur.Offset) ? r.Offset : cur.Offset;
+                cur.Length = cur.End - offset;
+                cur.Offset = offset;
+                return;
+            }
+            else // if (r.End > cur.End)
+            {
+                var offset = (r.Offset < cur.Offset) ? r.Offset : cur.Offset;
+                var end = r.End;
+                int i;
+                for(i = idx + 1; i < count; i++)
+                    if (_ranges[i].End > r.End)
+                        break;
+                if (i == count)
+                    _ranges.RemoveRange(idx + 1, count - idx - 1);
+                else
+                {
+                    end = _ranges[i].End;
+                    _ranges.RemoveRange(idx + 1, i - idx + 1 - 1);
+                }
+                _ranges[idx].Offset = offset;
+                _ranges[idx].Length = end - offset;
+            }
+        }
+
+        public override Task FlushAsync(System.Threading.CancellationToken cancellationToken)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override Task WriteAsync(byte[] buffer, int offset, int count, System.Threading.CancellationToken cancellationToken)
+        {
+            throw new NotSupportedException();
+        }
+    }
+
+    public class Range : IComparable<Range>
+    {
+        public long Offset;
+        public long Length;
+
+        public Range(long offset, long length)
+        {
+            Offset = offset;
+            Length = length;
+        }
+
+        public long End { get { return Offset + Length; } }
+
+        public bool Contains(Range r)
+        {
+            if (r.Offset < Offset || r.End > End)
+                return false;
+            return true;
+        }
+
+        public bool Contains(long pos, long size)
+        {
+            if (pos < Offset || pos + size > End)
+                return false;
+            return true;
+        }
+
+        public bool Contains(long pos)
+        {
+            if (pos < Offset || pos >= End)
+                return false;
+            return true;
+        }
+
+        #region IComparable implementation
+
+        public int CompareTo(Range other)
+        {
+            return (int)(Offset - other.Offset);
+        }
+
+        #endregion
+    }
+}
+
---
+++ HttpStream.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace HttpStream
+{
+	public class HttpStream : CacheStream
+	{
+		Uri _uri;
+		HttpClient _httpClient;
+		bool _ownHttpClient;
+		public long Size { get; private set; }
+		public bool SizeAvailable { get; private set; }
+		public bool InspectionFinished { get; private set; }
+		public DateTime LastModified { get; private set; }
+		public int BufferingSize { get; set; }
+
+		public HttpStream(Uri uri) : this(uri, new MemoryStream(), true)
+		{
+		}
+
+		public HttpStream(Uri uri, Stream cache, bool ownStream) : this(uri, cache, ownStream, new HttpClient())
+		{
+		}
+
+		public HttpStream(Uri uri, Stream cache, bool ownStream, HttpClient httpClient) : base(cache, ownStream)
+		{
+			BufferingSize = 1024 * 1024;
+			Size = long.MaxValue;
+			_uri = uri;
+			_httpClient = httpClient;
+			if (_httpClient == null)
+			{
+				_httpClient = new HttpClient();
+				_ownHttpClient = true;
+			}
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			base.Dispose(disposing);
+			if (disposing && _ownHttpClient)
+			{
+				_httpClient.Dispose();
+			}
+		}
+
+		public override long GetStreamLengthOrFail(long defValue)
+		{
+			return SizeAvailable ? Size : defValue;
+		}
+
+		public override async Task<Range> LoadAsync(Stream stream, long pos, int count, CancellationToken cancellationToken)
+		{
+			if(count == 0)
+				return new Range(pos, 0);
+
+			// confirm block unit access
+			long lowfilter = BufferingSize - 1;
+			long begPos = pos & ~lowfilter;
+			long endPos = (pos + count + lowfilter) & ~lowfilter;
+			pos &= ~lowfilter;
+			if(SizeAvailable && endPos > Size)
+				endPos = Size;
+			count = (int)(endPos - begPos);
+
+			var req = new HttpRequestMessage(HttpMethod.Get, _uri);
+			// Use "Range" header to sepcify the data offset and size
+			req.Headers.Add("Range", string.Format("bytes={0}-{1}", begPos, begPos + count - 1));
+
+			// post the request
+			var res = await _httpClient.SendAsync(req, cancellationToken);
+			Debug.WriteLine(string.Format("HTTP Status: {0} for bytes={1}-{2}", res.StatusCode, begPos, begPos + count - 1));
+
+			if(!res.IsSuccessStatusCode)
+				throw new Exception(string.Format("HTTP Status: {0} for bytes={1}-{2}", res.StatusCode, begPos, begPos + count - 1));
+
+			// retrieve the resulting Content-Range
+			bool getRanges = true;
+			long begin = 0, end = long.MaxValue;
+			long size = long.MaxValue;
+			try
+			{
+				// 206
+                var range = res.Content.Headers.GetValues("Content-Range").First();
+				Debug.WriteLine(range);
+				var m = Regex.Match(range, @"bytes\s+([0-9]+)-([0-9]+)/(\w+)");
+				begin = long.Parse(m.Groups[1].Value);
+				end = long.Parse(m.Groups[2].Value);
+				size = end - begin + 1;
+
+				if(!SizeAvailable)
+				{
+					var sz = m.Groups[3].Value;
+					if (sz != "*")
+					{
+						Size = long.Parse(sz);
+						SizeAvailable = true;
+					}
+				}
+
+                Debug.WriteLine(string.Format("Req: {0}-{1} -> Res: {2} (of {3})", begin, end, size, Size));
+			}
+			catch
+			{
+				try
+				{
+					// In some case, there's no Content-Range but Content-Length
+					// instead.
+					getRanges = false;
+					begin = 0;
+                    Size = end = size = long.Parse(res.Content.Headers.GetValues("Content-Length").First());
+                    SizeAvailable = true;
+                    Debug.WriteLine(string.Format("Req: {0}-{1} -> Res: {2} (of {3})", begin, end, size, Size));
+				}
+				catch
+				{
+					Debug.WriteLine("No Content-Length...");
+				}
+			}
+
+			try
+			{
+				LastModified = parseLastModified(res);
+			}
+			catch
+			{
+			}
+			InspectionFinished = true;
+
+			//if(size > 0xffffffffULL)
+			//    celThrow2(errOperationFailed, "Data size too large!");
+
+			var s = await res.Content.ReadAsStreamAsync();
+
+			int size32 = (int)size;
+			stream.Position = begin;
+			var buf = new byte[BufferingSize];
+			var copied = 0;
+			while (size32 > 0)
+			{
+				var bytes2Read = Math.Min(size32, BufferingSize);
+				var bytesRead = await s.ReadAsync(buf, 0, bytes2Read, cancellationToken);
+				if (bytesRead == 0)
+					break;
+
+				await stream.WriteAsync(buf, 0, bytesRead, cancellationToken);
+				size32 -= bytesRead;
+				copied += bytesRead;
+			}
+
+			if(!SizeAvailable && !getRanges)
+			{
+				Size = copied;
+				SizeAvailable = true;
+			}
+
+			return new Range(begin, copied);
+		}
+
+		DateTime parseLastModified(HttpResponseMessage res)
+		{
+            return parseDateTime(res.Content.Headers.GetValues("Last-Modified").First());
+		}
+
+		DateTime parseDateTime(string dateTime)
+		{
+			return DateTime.ParseExact(dateTime,
+				"ddd, dd MMM yyyy HH:mm:ss 'UTC'",
+				CultureInfo.InvariantCulture.DateTimeFormat,
+				DateTimeStyles.AssumeUniversal);
+		}
+	}
+}
+
