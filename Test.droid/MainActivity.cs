24a99ad5c9c2bdffc75bb659fc8d082b375ea639
Takashi Kawasaki <espresso3389@gmail.com>
2014-12-20 20:25:58 +0900

    Changes directory structure.

---
+++ Test.droid/MainActivity.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+using Android.Graphics;
+using System.Threading.Tasks;
+
+namespace Test
+{
+	[Activity (Label = "HttpStreamTest", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button> (Resource.Id.myButton);
+			
+			button.Click += delegate {
+				button.Text = string.Format ("{0} Clicks!", count++);
+			};
+
+            loadAsync();
+		}
+
+        async Task loadAsync()
+        {
+            var imageView = FindViewById<ImageView>(Resource.Id.imageView1);
+
+            var uri = new Uri(@"https://dl.dropboxusercontent.com/u/150906/trc_m.jpg");
+            var httpStream = new HttpStream.HttpStream(uri);
+
+            var bmp = await BitmapFactory.DecodeStreamAsync(httpStream);
+
+            imageView.SetImageBitmap(bmp);
+        }
+	}
+}
+
+
